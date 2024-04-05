7f7bc828a37a2feea7e8f130eba3dc82c4473b7e
tebogoyungmercykay <u20748052@tuks.co.za>
2024-04-05 11:41:20 +0000

    Implementation for The countDivisors Task

--- Functional-Programming/practical_2.rkt
+++ Functional-Programming/practical_2.rkt
@@ -13,3 +13,25 @@
 (newline)
 
 ; Answer from terminal :  32.182857142857145
+
+; Task 2
+
+(define (countDivisors number lst)
+  (define (is-divisor? n divisor)
+    (= (remainder n divisor) 0))
+ 
+  (define (count-divisors-helper lst count)
+    (cond
+      ((null? lst) count)
+      ((is-divisor? number (car lst))
+        (count-divisors-helper (cdr lst) (+ count 1)))
+      (else
+        (count-divisors-helper (cdr lst) count))))
+ 
+  (count-divisors-helper lst 0))
+
+; Example usage:
+(display (countDivisors 10 '(1 20 30 2 5 40 10 60)))
+(newline)
+
+; Answer from terminal : 4
