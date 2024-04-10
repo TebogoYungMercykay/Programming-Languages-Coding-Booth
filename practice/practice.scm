f86433f722e29d2e8923debd1df41069576802dc
tebogoyungmercykay <u20748052@tuks.co.za>
2024-04-10 19:54:37 +0000

    Duplicating Elements In A List That Are Not Equal to The Paramenter

--- practice/practice.scm
+++ practice/practice.scm
@@ -89,3 +89,19 @@
 (newline)
 (display (countDivisors 60 '(1 20 30)))
 (newline)
+
+
+(define (duplicateValues number listAtoms)
+  (if (null? listAtoms)
+    '()
+    (cons
+      (car listAtoms)
+      (if (eqv? number (car listAtoms))
+        (duplicateValues number (cdr listAtoms))
+        (cons (car listAtoms) (duplicateValues number (cdr listAtoms)))
+      )
+    )
+  )
+)
+
+(display (duplicateValues 'A '(A B C D E A V Y)))
