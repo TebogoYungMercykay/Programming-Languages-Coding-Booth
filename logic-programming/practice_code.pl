a34946cbd3ad56a4c14b3be9008627c2ab9658cd
tebogoyungmercykay <u20748052@tuks.co.za>
2024-04-21 15:11:54 +0000

    (practie) Summing All Natural Numbers In A List

--- logic-programming/practice_code.pl
+++ logic-programming/practice_code.pl
@@ -4,3 +4,27 @@ parent(john, ann).
 
 % Defining rules
 sibling(X, Y) :- parent(Z, X), parent(Z, Y), X \= Y.
+
+
+
+% Helper method
+positive(X) :- X >= 1.
+
+% Returning Zero For Empty List
+addPositives([], 0).
+
+% Adding All Members Which Are Natural numbers
+addPositives(List, X) :- 
+    member(H, List), 
+    positive(H), 
+    select(H, List, Rest), 
+    addPositives(Rest, X1), 
+    X is X1 + H.
+
+% Ignoring All Members Which Are Not Natural numbers
+addPositives(List, X) :- 
+    member(H, List), 
+    \+ positive(H), 
+    select(H, List, Rest), 
+    addPositives(Rest, X).
+
