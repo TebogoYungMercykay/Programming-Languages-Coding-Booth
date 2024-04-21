0437f517ca1ecea34a6210383586aad9da00b4ff
tebogoyungmercykay <u20748052@tuks.co.za>
2024-04-21 14:54:28 +0000

    Returning the Sum of all Positive Values in the List

--- logic-programming/practical_3.pl
+++ logic-programming/practical_3.pl
@@ -1,3 +1,6 @@
+% TASK 1
+
+
 % Defining Facts: Marriage and Pet Ownership
 married(peter, mary).
 married(lilly, joseph).
@@ -17,8 +20,32 @@ household(X, Y) :- married(Y, X).
 householdPet(O1, O2, P) :- household(O1, O2), (ownsPet(O1, P) ; ownsPet(O2, P)).
 
 % A pet is a wandering pet if it is a household pet of two different households
-wanderingPet(P) :- 
-    householdPet(O1, O2, P), 
-    householdPet(O3, O4, P), 
-    O1 \= O3, O2 \= O4.
+wanderingPet(P) :- householdPet(O1, O2, P), householdPet(O3, O4, P),
+    (O1 \= O3, O2 \= O4, O1 \= O4, O2 \= O3).
 
+
+% TASK 2
+
+
+% Helper method
+positive(X) :- X >= 1.
+
+% Sum whem list is empty
+addPositives([], 0).
+
+% If a member is positive, add it to the sum of rest of list
+addPositives(List, X) :- member(H, List), positive(H),
+    append(_, [H|T], List),
+    addPositives(T, X1),
+    X is X1 + H.
+
+% If a member is not positive, ignore it and sum rest of list
+addPositives(List, X) :- member(H, List), \+ positive(H),
+    append(_, [H|T], List),
+    addPositives(T, X).
+
+
+% TASK 2
+
+
+% Helper method
