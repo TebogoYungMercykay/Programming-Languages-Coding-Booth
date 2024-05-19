e12951401899b8c90d2d3066efb2c00bff9e59b2
tebogoyungmercykay <u20748052@tuks.co.za>
2024-05-19 23:49:43 +0000

    (p6) Perl: Word Count Scripts Implementation

--- imperative-scripting-languages/practical_6.pl
+++ imperative-scripting-languages/practical_6.pl
@@ -5,23 +5,23 @@ use warnings;
 if (@ARGV != 2) {
     print "Usage: perl script.pl <filename> <word>\n";
     exit(1);
-}
-
-my ($filename, $word) = @ARGV;
-
-if (-e $filename) {
-    open my $fh, '<', $filename or die "Could not open '$filename' $!\n";
-    my $fileContent = do { local $/; <$fh> };
-    close $fh;
-
-    $fileContent = lc $fileContent;
-    $fileContent =~ s/[^a-z]//g;
-
-    my $reversedContent = reverse $fileContent;
-
-    my $count = () = $reversedContent =~ /$word/g;
-
-    print "Matches: $count\n";
 } else {
-    print "File not found.\n";
+    my ($filename, $word) = @ARGV;
+    
+    if (-e $filename) {
+        open my $fh, '<', $filename or die "Could not open '$filename' $!\n";
+        my $fileContent = do { local $/; <$fh> };
+        close $fh;
+    
+        $fileContent = lc $fileContent;
+        $fileContent =~ s/[^a-z]//g;
+    
+        my $reversedContent = reverse $fileContent;
+    
+        my $count = () = $reversedContent =~ /$word/g;
+    
+        print "Matches: $count\n";
+    } else {
+        print "File not found.\n";
+    }
 }
